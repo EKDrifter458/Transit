66cf06629dc5fe4bf5192bff9e196ee3913e7857
EKDrifter458 <98769437+EKDrifter458@users.noreply.github.com>
2022-04-25 21:14:35 -0400

    Update TNode.java

--- TNode.java
+++ TNode.java
@@ -1,14 +1,6 @@
 package transit;
 
-/**
- * This class contains a Transportation Node, with an int
- * representing location, a next pointer representing horizontal
- * movement, and a down pointer representing a slower mode of 
- * transport
- * 
- * @author Ishaan Ivaturi
- * @author Prince Rawal
- */
+
 public class TNode {
 	private int location;
 	private TNode next;
@@ -21,12 +13,12 @@ public class TNode {
 	}
 
 	public TNode() {
-		// No arguments sets location to 0
+		
 		this(0, null, null);
 	}
 
 	public TNode(int l){
-		// Int argument sets location
+		
 		this(l, null, null);
 	}
 
